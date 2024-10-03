e135d2f3913f6cb81d46083106e0ca955a1cf436
Rafizan <rafi.syawalazmi@gmail.com>
2024-10-04 05:41:13 +0700

    Feat: Added Gauss and Gauss Jordan Elimination
    Added Gauss and Gauss Jordan Elimination

---
+++ bin/GaussJordan.java
@@ -0,0 +1,82 @@
+package bin;
+
+public class GaussJordan {
+
+    public int FindPotentialLeadingOne(Matrix m, int idx) {
+        for (int i = 0; i < m.rowEff; i++) {
+            for (int j = 0; j < m.colEff; j++) {
+                if (j < idx && m.mem[i][j] != 0) {
+                    break;
+                }
+                if (j == idx && m.mem[i][j] != 0) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public Matrix GaussElimination(Matrix m) {
+        int leadingIdx = 0;
+        int targetIdx;
+        for (int i = 0; i < m.rowEff; i++) {
+            /* Swap baris ke-i dengan baris yang memiliki non zero di kolom ke-leadingIdx */
+            while (true) {
+                targetIdx = FindPotentialLeadingOne(m, leadingIdx);
+                if (targetIdx != -1) {
+                    break;
+                }
+                if (++leadingIdx == m.colEff) {
+                    return m;
+                }
+            }
+            m.RowSwap(i, targetIdx);
+            
+            /* Kalikan baris ke-i agar mendapatkan baris yang memiliki 1 utama */
+            m.RowMultiply(i, 1 / m.mem[i][leadingIdx]);
+
+            /* Kurangkan baris yang bukan ke-i dengan k * i agar baris tersebut memiliki nilai 0 pada kolom ke-leadingIdx */
+            for (int j = i + 1; j < m.rowEff; j++) {
+                m.RowAddition(j, i, -m.mem[j][leadingIdx]);
+            }
+
+            leadingIdx++;
+        }
+
+        return m;
+    }
+
+    public Matrix GaussJordanElimination(Matrix m) {
+        int leadingIdx = 0;
+        int targetIdx;
+        for (int i = 0; i < m.rowEff; i++) {
+            /* Swap baris ke-i dengan baris yang memiliki non zero di kolom ke-leadingIdx */
+            while (true) {
+                targetIdx = FindPotentialLeadingOne(m, leadingIdx);
+                if (targetIdx != -1) {
+                    break;
+                }
+                if (++leadingIdx == m.colEff) {
+                    return m;
+                }
+            }
+            if (targetIdx != i) {
+                m.RowSwap(i, targetIdx);
+            }
+
+            /* Kalikan baris ke-i agar mendapatkan baris yang memiliki 1 utama */
+            m.RowMultiply(i, 1 / m.mem[i][leadingIdx]);
+            
+            /* Kurangkan baris yang bukan ke-i dengan k * i agar baris tersebut memiliki nilai 0 pada kolom ke-leadingIdx */
+            for (int j = 0; j < m.rowEff; j++) {
+                if (j != i) {
+                    m.RowAddition(j, i, -m.mem[j][leadingIdx]);
+                }
+            }
+
+            leadingIdx++;
+        }
+
+        return m;
+    }
+}
