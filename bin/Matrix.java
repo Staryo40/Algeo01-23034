b3fb03dca70c4c92ae6ae672a7fc7ec47f63e63e
Aryo Wisanggeni <arrrryow@gmail.com>
2024-10-03 23:53:16 +0700

    refactor: change data type of matrix element from integer to float

--- bin/Matrix.java
+++ bin/Matrix.java
@@ -1,14 +1,15 @@
 package bin;
 
 public class Matrix {
-    int[][] mem;
+    float[][] mem;
     int rowEff;
     int colEff;
 
+    // Constructor
     public Matrix(int nRow,int nCol){
         rowEff = nRow;
         colEff = nCol;
-        mem = new int[nRow][nCol]; 
+        mem = new float[nRow][nCol]; 
         for (int i = 0; i < nRow; i++){
             for (int j = 0; j < nCol; j++){
                 mem[i][j] = 0;
@@ -16,16 +17,34 @@ public class Matrix {
         }
     }   
 
+    // Function to help format float
+     public static String formatFloat(float value) {
+        if (value == Math.floor(value)) {
+            return String.format("%.0f", value);
+        } else {
+            return String.format("%.1f", value);
+        }
+    }
+
+    // Procedure to print matrix
     public void printMatrix(){
         for (int i = 0; i < rowEff; i++){
             for (int j = 0; j < colEff; j++){
-                System.out.print(mem[i][j]);
+
+                System.out.print(formatFloat((mem[i][j])));
+
                 if (j < colEff-1){
                     System.out.print(" ");
                 }
+
             }
             System.out.println("");
         }
+    }
+
+    // Function to get element from row and column
+    public float getElmt(Matrix m, int row, int col){
+        return m.mem[row][col];
     }
 }
 
