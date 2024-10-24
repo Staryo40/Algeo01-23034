35f75bd5ef11ffa59bb3b64dd8b15fbc187c4ded
Rafizan <rafi.syawalazmi@gmail.com>
2024-10-24 13:44:29 +0700

    Matrix Creator
    Hilbert and Identity

---
+++ bin/MatrixOutput.java
@@ -0,0 +1,10 @@
+package bin;
+
+public class MatrixOutput {
+
+    // Fungsi yang menentukan hasil SPL matrix m
+    // Prekondisi : Matrix m adalah matris eselon baris tereduksi
+    public static void OutputSPL(Matrix m) {
+        
+    }
+}
---
+++ bin/MatrixType.java
@@ -0,0 +1,24 @@
+package bin;
+
+public class MatrixType {
+
+    // Fungsi yang mengembalikan matrix identitas n x n
+    public static Matrix Identity(int n) {
+        Matrix m = new Matrix(n, n);
+        m.GetIdentity();
+        return m;
+    }
+
+    // Fungsi yang mengembalikan matrix hilbert n x n
+    public static Matrix Hilbert(int n) {
+        Matrix m = new Matrix(n , n);
+        
+        for (int i = 0; i < m.rowEff; i++) {
+            for (int j = 0; j < m.colEff; j++) {
+                m.mem[i][j] = 1 / (i + j + 1);
+            }
+        }
+
+        return m;
+    }
+}
